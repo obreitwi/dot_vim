0066ae57799f3905b4148df393efc86ac03543cc
Oliver Breitwieser <oliver.breitwieser@gmail.com>
2015-09-30 12:28:33 +0200

    Fixed install issue

--- install.sh
+++ install.sh
@@ -2,7 +2,6 @@
 
 # Quick install script to setup all symlinks
 git clone https://github.com/gmarik/vundle.git bundle/vundle
-vim +BundleInstall +q!
 
 PREFIX=$HOME/.local
 # SRCFLD=$(dirname $(realpath $0))
@@ -21,5 +20,4 @@ symlink $SRCFLD/vimpager/repo/vimcat $PREFIX/bin/vcat
 symlink $SRCFLD/vimpager/repo/vimpager $PREFIX/bin/vimpager
 symlink $SRCFLD/vimpager/repo/vimpager.1 $PREFIX/share/man
 
-
-
+vim +BundleInstall +q!
