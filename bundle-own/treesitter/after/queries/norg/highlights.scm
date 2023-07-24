7bacdc4dfcb372821697d2967b5d7620c451c984
Oliver Breitwieser <oliver@breitwieser.eu>
2023-07-25 01:14:01 +0200

    Fix treesitter queries for neorg

--- bundle-own/treesitter/after/queries/norg/highlights.scm
+++ bundle-own/treesitter/after/queries/norg/highlights.scm
@@ -1,4 +1,18 @@
-; testing
-; "#STORY"  @comment
-; ((paragraph_segment) @comment (#vim-match? @comment "\v\s*#STORY.*$") (#set! conceal "SSS"))
-; ((paragraph_segment) @comment (#vim-match? @comment "\d\d:\d\d") (#set! conceal "TIMETIMETIME"))
+;; extends
+
+(
+ (unordered_list1
+   state: (detached_modifier_extension [(todo_item_undone) (todo_item_done) (todo_item_pending)])
+   content: (paragraph (paragraph_segment (inline_comment ("_open") ("_word" @task-id-tag) ("_word") ("_close")) @conceal (#set! conceal "")))
+ )
+ (#match? @task-id-tag "#taskid")
+)
+
+; (
+ ; (unordered_list1
+   ; state: (detached_modifier_extension [(todo_item_undone) (todo_item_done) (todo_item_pending)])
+   ; content: (paragraph (paragraph_segment (("_word" @todo-id-tag) ("_word" @todo-content) @conceal (#set! conceal ""))))
+ ; )
+ ; (#match? @todo-id-tag "#taskid")
+ ; (#match? @todo-content "^[a-zA-Z].*$")
+; )
